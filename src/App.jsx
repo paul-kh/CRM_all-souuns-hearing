8b3e6e38a56f4efff12b5579ff8dbe77d766fc5e
Paul Chheang <phalcheat@gmail.com>
2024-08-10 11:31:55 -0400

    Added initialproject files

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <>
+      <header>
+        <h1>All Sounds Hearing CRM System</h1>
+      </header>
+      <nav>
+        <ul>
+          <li>
+            <a href="">Create customer account</a>
+          </li>
+          <li>
+            <a href="">Create Order</a>
+          </li>
+          <li>
+            <a href="">Create Invoice</a>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h3>Main content</h3>
+      </main>
+    </>
+  );
+}
+
+export default App;
