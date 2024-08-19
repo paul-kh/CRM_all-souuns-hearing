1616ffa69cc5dc07188c264e8e809bdd965829ba
Paul Chheang <phalcheat@gmail.com>
2024-08-18 21:51:28 -0400

    Created page for Work Order Search

--- src/pages/SearchWorkOrder.jsx
+++ src/pages/SearchWorkOrder.jsx
@@ -6,7 +6,43 @@ export default function SearchWorkOrder() {
       </div>
 
       <div className="page__details">
-        <p>Coming soon...</p>
+        <form onSubmit={() => {}}>
+          <div className="control-group__row">
+            <div className="input-group">
+              <label htmlFor="workorder-search-by">Search By</label>
+              <select
+                id="workorder-search-by"
+                type="text"
+                name="search-by"
+                required
+              >
+                <option value="workorder-number">Work Order Number</option>
+                <option value="customer-account-number">
+                  Customer Account Number
+                </option>
+              </select>
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="workorder-search-value">Value</label>
+              <input
+                id="workorder-search-value"
+                type="text"
+                name="workorder-search-value"
+                required
+              />
+            </div>
+          </div>
+
+          <p className="form-actions p-t-1">
+            <button type="reset" className="button button-flat">
+              Reset
+            </button>
+            <button type="submit" className="button">
+              Search
+            </button>
+          </p>
+        </form>
       </div>
     </main>
   );
