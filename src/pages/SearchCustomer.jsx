091392a78df2f1e68b647d80d3d15b339e07ebca
Paul Chheang <phalcheat@gmail.com>
2024-08-18 21:42:35 -0400

    Created page for Customer Search

--- src/pages/SearchCustomer.jsx
+++ src/pages/SearchCustomer.jsx
@@ -6,7 +6,37 @@ export default function SearchCustomer() {
       </div>
 
       <div className="page__details">
-        <p>Coming soon...</p>
+        <form onSubmit={() => {}}>
+          <div className="control-group__row">
+            <div className="input-group">
+              <label htmlFor="search-by">Search By</label>
+              <select id="search-by" type="text" name="search-by" required>
+                <option value="account-number">Account Number</option>
+                <option value="customer-name">Customer Name</option>
+                <option value="phone-number">Phone number</option>
+              </select>
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="search-value">Value</label>
+              <input
+                id="search-value"
+                type="text"
+                name="search-value"
+                required
+              />
+            </div>
+          </div>
+
+          <p className="form-actions p-t-1">
+            <button type="reset" className="button button-flat">
+              Reset
+            </button>
+            <button type="submit" className="button">
+              Search
+            </button>
+          </p>
+        </form>
       </div>
     </main>
   );
