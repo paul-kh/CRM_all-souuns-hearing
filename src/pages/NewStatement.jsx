024e9db32c31d3be26ebbd673f6b0128ded8499c
Paul Chheang <phalcheat@gmail.com>
2024-08-18 21:58:10 -0400

    Created page for New Statement

--- src/pages/NewStatement.jsx
+++ src/pages/NewStatement.jsx
@@ -6,7 +6,38 @@ export default function NewStatement() {
       </div>
 
       <div className="page__details">
-        <p>Coming soon...</p>
+        <form onSubmit={() => {}}>
+          <div className="control-group__row">
+            <div className="input-group">
+              <label htmlFor="account-number">Customer Account Number</label>
+              <input
+                id="account-number"
+                type="number"
+                name="account-number"
+                required
+              />
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="from-date">From Date</label>
+              <input id="from-date" type="date" name="from-date" required />
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="to-date">To Date</label>
+              <input id="to-date" type="date" name="to-date" required />
+            </div>
+          </div>
+
+          <p className="form-actions p-t-1">
+            <button type="reset" className="button button-flat">
+              Reset
+            </button>
+            <button type="submit" className="button">
+              Generate Statement
+            </button>
+          </p>
+        </form>
       </div>
     </main>
   );
