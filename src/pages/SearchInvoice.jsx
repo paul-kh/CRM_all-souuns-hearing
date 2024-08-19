d70b4b250dcdf4bd9817605e0e90ba970afe7f0e
Paul Chheang <phalcheat@gmail.com>
2024-08-18 21:48:27 -0400

    Created page for Search Invoice

--- src/pages/SearchInvoice.jsx
+++ src/pages/SearchInvoice.jsx
@@ -6,7 +6,41 @@ export default function SearchInvoice() {
       </div>
 
       <div className="page__details">
-        <p>Coming soon...</p>
+        <form onSubmit={() => {}}>
+          <div className="control-group__row">
+            <div className="input-group">
+              <label htmlFor="invoice-search-by">Search By</label>
+              <select
+                id="invoice-search-by"
+                type="text"
+                name="invoice-search-by"
+                required
+              >
+                <option value="invoice-number">Invoice Number</option>
+                <option value="account-number">Customer Account Number</option>
+              </select>
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="invoice-search-value">Value</label>
+              <input
+                id="invoice-search-value"
+                type="text"
+                name="invoice-search-value"
+                required
+              />
+            </div>
+          </div>
+
+          <p className="form-actions p-t-1">
+            <button type="reset" className="button button-flat">
+              Reset
+            </button>
+            <button type="submit" className="button">
+              Search
+            </button>
+          </p>
+        </form>
       </div>
     </main>
   );
